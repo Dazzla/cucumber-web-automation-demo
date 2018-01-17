571d461089b7a4e41ed7b905679aad636099e9b6
Dazzla <dazzlebown@gmail.com>
2018-01-17 11:58:42 +0000

    Adds report directory, .gitignores images

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,8 @@
 BASE_URL = 'http://automationpractice.com/'
 USERNAME = 'db@mailinator.com'
 TEST_COMMENT = 'TEST COMMENT 001'
+SCREENSHOT_DIRECTORY = 'features/reports/screenshots'
+
 require 'page-object'
 require 'cucumber'
 require 'rspec/matchers'
